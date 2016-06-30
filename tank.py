0fb7f97d0d26d8da97a0349322a2e754b73bd633
sgmonusg <sagargupta_monu@yahoo.co.in>
2016-06-30 13:41:45 +0530

    "added tank.py"

---
+++ tank.py
@@ -0,0 +1,24 @@
+#tank System
+
+#tank display unit
+
+#showing rpm,speed,gear,engine power in use
+
+#movement of turrent
+
+#movement of cover to protect the solar plates
+
+#override system for commander and gunner
+
+#fast and slow movement of turret 
+
+
+#as of now we have are not collaborating it with buttons
+
+
+so we have a menu system
+
+
+class tank:
+	def __init__(self):
+		print "tank class started"
