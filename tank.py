8cf7f09d5d5c93f61ba5db5d7a71d538675a39de
sgmonusg <sagargupta_monu@yahoo.co.in>
2016-06-30 16:32:45 +0530

    "added driver commander tank display"

--- tank.py
+++ tank.py
@@ -19,6 +19,50 @@
 so we have a menu system
 
 
-class tank:
+class tank_display:
 	def __init__(self):
 		print "tank class started"
+
+	def rpm(self):
+		print "get the rpm"
+	def speed(self):
+		print "get the speed"
+	def gear(self):
+		print "get the gear"
+	def engine_power(self):
+		print "get the engine power"
+
+
+class commander:
+	def __init__(self):
+		print "commander class started"
+
+	def get_turrent_angle(self):
+		print "get the current tank turret angle"
+
+	def get_commander_viewpoint(self):
+		print "get the angle at which commander is viewing"
+
+	def change_turret_angle(self):
+		print "change the angle of turrent from current to the view angle"
+
+	def get_gunner_view(self):
+		print "show on the screen the view that the gunner is currently seeing"
+
+	def override_gunner_angle(self):
+		print "irrespective of what gunner is aimming for the turret moves on commander command"
+	
+	def get_gunner_angle(self):
+		print "show gunner view angle"
+
+class driver:
+	def __init__(self):
+		print "driver class started"
+
+	def give_fire_warning(self):
+		print "all the chamber red light starts to blink"
+
+	def claok_sun_panels(self):
+		print "covered the solar panels to reduce damage"
+
+	def 
